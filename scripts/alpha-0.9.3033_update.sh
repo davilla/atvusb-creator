06a5799205e41c1a2f538ebe55ceca0b89998ec6
davilla@4pi.com <davilla@4pi.com@a383d182-5b53-0410-acfb-4f71ec231b6c>
2008-10-01 04:34:54 +0000

    testing boxee install

---
+++ scripts/alpha-0.9.3033_update.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+DISKIMAGE=$1
+
+echo "Installing from diskimage $DISKIMAGE"
+# check that disk-image exists
+if [ -e $DISKIMAGE ]; then
+  PW="frontrow"
+
+  # check and store OSBoot read/write settings
+  REMOUNT=0
+  # Check if / is mounted read only
+  if mount | grep ' on / '  | grep -q 'read-only'; then
+    REMOUNT=1
+    /sbin/mount -uw /
+  fi
+
+  # install xbmc app
+  echo $PW | sudo -S hdiutil attach $DISKIMAGE 
+  echo $PW | sudo -S installer -pkg /Volumes/boxee-tiger3/boxee-tiger3.mpkg -target /
+  echo $PW | sudo -S hdiutil detach /Volumes/boxee-tiger3
+
+  # handle any post-install items here
+
+  # clean up
+  if [ -e /Users/Frontrow/Movies/boxee-tiger3 ]; then
+  # something makes this symlink during install so zap it.
+  echo $PW | sudo -S rm /Users/Frontrow/Movies/boxee-tiger3
+  fi
+
+  # restore OSBoot read/write settings
+  if [ "$REMOUNT" = "1" ]; then
+    /sbin/mount -ur /
+  fi
+
+  exit 0
+fi
+echo "Failed to find diskimage $DISKIMAGE"
+exit -1
