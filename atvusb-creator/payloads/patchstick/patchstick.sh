85aeb06da79f185c0a425e95d481f95c632d981a
bdwilson <bdwilson@a383d182-5b53-0410-acfb-4f71ec231b6c>
2008-10-02 15:05:16 +0000

    minor information changes

--- atvusb-creator/payloads/patchstick/patchstick.sh
+++ atvusb-creator/payloads/patchstick/patchstick.sh
@@ -32,6 +32,7 @@ if [ -d "/OSBoot/dev" ]; then
 		for script in $( find /payloads/ -name install.sh -print ); do
  			/bin/bash "${script}" 
 		done
+		echo "        * script installation successful!"
 	else 
 		echo "        * error mounting Media partition"
 	fi	
@@ -47,7 +48,11 @@ sync
 umount /OSBoot/mnt
 umount /OSBoot
 echo "        * Please unplug your Apple TV to reboot/reset the device."
-echo "        * To connect to your Apple TV, plug in device, wait 3 minutes, then open"
-echo "          Terminal or an ssh client (putty), and type: ssh frontrow@appletv.local"
+echo
+echo "        * If you received no errors above, then you can plug in your Apple TV, "
+echo "          wait 3 minutes, then open Terminal and type: ssh frontrow@appletv.local " 
 echo "        * When prompted for a password, enter: frontrow"
+echo
+echo "        * For other ssh clients (putty, etc):  "
+echo "          Hostname: appletv.local  Username: frontrow  Password: frontrow"
 sleep 100000
