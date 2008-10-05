4f425921648e8cfe1ac23ad7f7e7c58d30e03a44
davilla@4pi.com <davilla@4pi.com@a383d182-5b53-0410-acfb-4f71ec231b6c>
2008-10-05 16:41:31 +0000

    [Linux] add _get_device, Issue 37, thanks cck197

--- atvusb-creator/liveusb/creator.py
+++ atvusb-creator/liveusb/creator.py
@@ -16,6 +16,9 @@
 # permission of Red Hat, Inc.
 #
 # Author(s): Luke Macken <lmacken@redhat.com>
+# 
+# ATV-USB-Creator fork:
+#            Scott Davilla <davilla@4pi.com>
 
 """
 Our main LiveUSBCreator module.
@@ -441,6 +444,10 @@ class LinuxLiveUSBCreator(LiveUSBCreator):
     def detect_removable_drives(self):
         """ Detect all removable USB storage devices using HAL via D-Bus """
         import dbus
+        def _get_device(device):
+            dev_obj = self.bus.get_object("org.freedesktop.Hal", device)
+            return dbus.Interface(dev_obj, "org.freedesktop.Hal.Device")
+
         self.drives = {}
         self.bus = dbus.SystemBus()
         hal_obj = self.bus.get_object("org.freedesktop.Hal",
@@ -455,7 +462,7 @@ class LinuxLiveUSBCreator(LiveUSBCreator):
             devices = self.hal.FindDeviceByCapability("storage")
 
         for device in devices:
-            dev = self._get_device(device)
+            dev = _get_device(device)
             if self.opts.force or dev.GetProperty("storage.bus") == "usb" and \
                dev.GetProperty("storage.removable"):
                 if dev.GetProperty("block.is_volume"):
@@ -465,7 +472,7 @@ class LinuxLiveUSBCreator(LiveUSBCreator):
                     children = self.hal.FindDeviceStringMatch("info.parent",
                                                               device)
                     for child in children:
-                        child = self._get_device(child)
+                        child = _get_device(child)
                         if child.GetProperty("block.is_volume"):
                             self._add_device(child)
                             break
